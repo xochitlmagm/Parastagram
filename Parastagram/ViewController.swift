56a6224ba83cf82f9d6ddd5492277c93d6eec78e
Xochitl Medina <medinaxochitl98@gmail.com>
2020-10-01 21:55:57 -0700

    Initial Commit

---
+++ Parastagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parastagram
+//
+//  Created by Xochitl Medina on 10/1/20.
+//  Copyright © 2020 Xochitl Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
